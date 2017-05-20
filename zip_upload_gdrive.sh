cb2f9ba156b93c6b522f5648a477eb09c638e7b5
adamgann <adamgann@buffalo.edu>
2017-05-20 15:15:07 -0400

    Initial commut of Zip gdrive script

---
+++ zip_upload_gdrive.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+TARGET_DIR='/home/adam/'
+ZIP_DIR='src'
+PARENT_ID='' #Fill this in from the Google Drive URL
+
+# Name the file using today's date
+PREFIX='backup_'
+DATE=`date +%Y_%m_%d_%H_%M`
+POSTFIX='.tar.gz'
+FILENAME=$PREFIX$DATE$POSTFIX
+
+# Notify that process is starting
+notify-send "Git backup process starting"
+
+# Zip the files 
+echo $FILENAME
+tar -zcf $FILENAME $TARGET_DIR$ZIP_DIR 
+
+# Upload To GDrive, Delete Local Copy
+gdrive upload --parent $PARENT_ID $FILENAME
+rm $FILENAME
+
+notify-send "Git backup process complete"
+
+
+
